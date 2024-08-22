6cf1f41ddaf53af7fad31ca789907c2dcacef93e
Santiago <santidurancanedo@gmail.com>
2024-08-21 20:03:33 -0500

    installation and introduction

---
+++ hello_reflex/hello_reflex/hello_reflex.py
@@ -0,0 +1,18 @@
+import reflex as rx 
+
+
+def index():
+    return rx.hstack(
+        rx.button(
+            "Decrement",
+            color_scheme="ruby",
+            on_click=State.decrement,
+        ),
+        rx.heading(State.count, font_size="2em"),
+        rx.button(
+            "Increment",
+            color_scheme="grass",
+            on_click=State.increment,
+        ),
+        spacing="4",
+    )
